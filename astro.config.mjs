678cffa3d777229ec9e9f30dfed2a2f129827d46
Nicholas Chase <nickscottchase@proton.me>
2024-09-18 12:34:35 -0700

    astro + clerk + cloudflare .env variables can't be found

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
-import clerk from '@clerk/astro';
+import clerk from "@clerk/astro";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [clerk()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
+  adapter: cloudflare(),
 });
